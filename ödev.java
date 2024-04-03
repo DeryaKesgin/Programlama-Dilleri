ad89baea9d3b6739d06399500d08851050eed830
DeryaKesgin <99399615+DeryaKesgin@users.noreply.github.com>
2024-04-03 20:20:16 +0300

    Create ödev.java

---
+++ ödev.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prog;
+
+
+
+
+    public class bagliListe {
+    int veri;
+    int adres;
+
+    public bagliListe(int veri, int adres) {
+        this.veri = veri;
+        this.adres = adres;
+    }
+}
